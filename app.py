2288cdd6b010733ca258f2b03ff181ea80dd7205
R-NK <right.rcon.f@outlook.com>
2018-10-02 14:23:42 +0900

    init

---
+++ app.py
@@ -0,0 +1,76 @@
+import os
+import json
+
+from azure.servicebus import ServiceBusService
+from azure.servicebus import Message
+
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
+)
+
+import settings
+
+app = Flask(__name__)
+
+host = os.getenv('HOST', 'http://localhost:8080')
+line_bot_api = LineBotApi(settings.CHANNEL_ACCESS_TOKEN, host)
+handler = WebhookHandler(settings.CHANNEL_SECRET)
+
+service_namespace = 'linebot-test'
+sbs = ServiceBusService(service_namespace,
+                        shared_access_key_name=settings.SHARED_ACCESS_KEY_NAME,
+                        shared_access_key_value=settings.SHARED_ACCESS_KEY)
+
+obachan_full = "https://rawgit.com/Wild-Family/line-bot/master/resource/obachan_full.jpg"
+obachan_thumb = "https://rawgit.com/Wild-Family/line-bot/master/resource/obachan_thumb.jpg"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.message.text == 'とって':
+        line_bot_api.reply_message(
+            event.reply_token, [
+                ImageSendMessage(original_content_url=obachan_full, preview_image_url=obachan_thumb),
+                TextSendMessage(text="オバチャンに任せとき！")
+            ]
+        )
+        sbs.create_queue('test')
+        msg_dict = {
+            'user_id': event.source.user_id,
+            'pic_type': event.message.text
+        }
+        msg_json = json.dumps(msg_dict, indent=4)
+        msg = Message(msg_json)
+        sbs.send_queue_message('test', msg)
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8000)
---
+++ settings.py
@@ -0,0 +1,24 @@
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+CHANNEL_SECRET = os.getenv('CHANNEL_SECRET', None)
+CHANNEL_ACCESS_TOKEN = os.getenv('CHANNEL_ACCESS_TOKEN', None)
+
+if CHANNEL_SECRET is None:
+    print('Specify CHANNEL_SECRET as environment variable.')
+    os.sys.exit(1)
+if CHANNEL_ACCESS_TOKEN is None:
+    print('Specify CHANNEL_ACCESS_TOKEN as environment variable.')
+    os.sys.exit(1)
+
+SHARED_ACCESS_KEY_NAME = os.getenv('SHARED_ACCESS_KEY_NAME', None)
+SHARED_ACCESS_KEY = os.getenv('SHARED_ACCESS_KEY', None)
+
+if SHARED_ACCESS_KEY_NAME is None:
+    print('Specify SHARED_ACCESS_KEY_NAME as environment variable.')
+    os.sys.exit(1)
+if SHARED_ACCESS_KEY is None:
+    print('Specify SHARED_ACCESS_KEY as environment variable.')
+    os.sys.exit(1)
